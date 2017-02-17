f1c1c5f0eba9b2ab90ac38b91e5c8c39f7c9a4c9
Matthew-Burfield <m1.burfield@gmail.com>
2017-02-17 13:19:01 +1000

    Linked up exercises

--- src/components/Exercise.js
+++ src/components/Exercise.js
@@ -1,14 +1,41 @@
 import React from 'react';
 
-const Exercise = props =>
-  <div>
-    {props.params.name}
-  </div>;
+const Exercise = (props) => {
+  const ex = props.exercise;
+
+  return (<div>
+    <p><b>Name:</b> {ex.name}</p>
+    <p><b>Sets:</b> {ex.sets}</p>
+    <p><b>Reps:</b> {ex.sets}</p>
+    {ex.selectedVariation !== '' && ex.variations.length > 0 &&
+      <p><b>Current Variation:</b> {ex.variations[ex.selectedVariation]}</p>
+    }
+    {ex.weight &&
+      <p><b>weight:</b> {ex.weight}</p>
+    }
+    <p><b>Time in between sets:</b> {ex.minInBetweenSets} min</p>
+  </div>);
+};
 
 Exercise.propTypes = {
-  params: React.PropTypes.shape({
+  exercise: React.PropTypes.shape({
     name: React.PropTypes.string.isRequired,
-  }).isRequired,
+    sets: React.PropTypes.number.isRequired,
+    reps: React.PropTypes.number.isRequired,
+    minInBetweenSets: React.PropTypes.number,
+    variations: React.PropTypes.arrayOf(React.PropTypes.string),
+    selectedVariation: React.PropTypes.number,
+    weight: React.PropTypes.number,
+    timeLengthOfExercise: React.PropTypes.number,
+  }),
+};
+
+Exercise.defaultProps = {
+  exercise: {
+    name: '',
+    sets: 0,
+    reps: 0,
+  },
 };
 
 export default Exercise;
--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import NavLink from './NavLink';
 
-const Exercises = (props, { children }) =>
+const Exercises = props =>
   <div>
-    <ul>
-      {props.routines[0].exercises.map((item) => {
+    <h2>Current Routine: {props.routines[0].name}</h2>
+    <div className="exerciseBreadCrumb">
+      {props.routines[0].exercises.map((item, index) => {
         const path = `/exercises/${item.name}`;
-        return <li key={item.name}><NavLink to={path}>{item.name}</NavLink></li>;
+        return (
+          <div key={item.name} className="exerciseBreadCrumbItemContainer">
+            <div className="exerciseBreadCrumbItem">
+              <NavLink to={path}>{item.name}</NavLink>
+            </div>
+            {(index + 1) < props.routines[0].exercises.length &&
+              <div>&gt;&gt;</div>
+            }
+          </div>
+        );
       })}
-    </ul>
-    {children}
+    </div>
+    {props.children && React.cloneElement(props.children, {
+      exercise: [...props.routines[0].exercises.filter(item => item.name === props.params.name)][0],
+    })}
   </div>;
 
 Exercises.propTypes = {
+  children: React.PropTypes.element,
+  params: React.PropTypes.shape({
+    name: React.PropTypes.string,
+  }),
   routines: React.PropTypes.arrayOf(React.PropTypes.shape({
     name: React.PropTypes.string.isRequired,
     exercises: React.PropTypes.arrayOf(React.PropTypes.shape({
@@ -30,6 +46,9 @@ Exercises.propTypes = {
 Exercises.defaultProps = {
   children: [],
   routines: [],
+  params: {
+    name: '',
+  },
 };
 
 export default Exercises;
