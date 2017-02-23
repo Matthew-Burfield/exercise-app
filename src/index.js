60fef275c85def0458a4f4ff2ef9d37de4818a97
Matthew-Burfield <m1.burfield@gmail.com>
2017-02-24 07:53:55 +1000

    Changed to hashHistory so I can deploy to production without needing a server

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, browserHistory, IndexRoute } from 'react-router';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import App from './App';
 import Dashboard from './components/Dashboard';
 import Exercises from './components/Exercises';
@@ -8,7 +8,7 @@ import Exercise from './components/Exercise';
 import './index.scss';
 
 ReactDOM.render(
-  <Router history={browserHistory}>
+  <Router history={hashHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={Dashboard} />
       <Route path="/exercises" component={Exercises}>
