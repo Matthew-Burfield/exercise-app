c171e19d9607b204d2eb1827359ae4937541ece1
Matthew-Burfield <m1.burfield@gmail.com>
2017-02-27 05:47:57 +1000

    first commit

--- src/index.js
+++ src/index.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import * as reducer from './reducers/reducer';
 import App from './App';
 import Dashboard from './components/Dashboard';
 import Exercises from './components/Exercises';
 import Exercise from './components/Exercise';
 import './index.scss';
 
+const store = createStore(reducer);
+
 ReactDOM.render(
-  <Router history={hashHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Dashboard} />
-      <Route path="/exercises" component={Exercises}>
-        <Route path="/exercises/:name" component={Exercise} />
+  <Provider store={store}>
+    <Router history={hashHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Dashboard} />
+        <Route path="/exercises" component={Exercises}>
+          <Route path="/exercises/:name" component={Exercise} />
+        </Route>
       </Route>
-    </Route>
-  </Router>,
+    </Router>
+  </Provider>,
   document.getElementById('root'),
 );
