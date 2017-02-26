eb36ba1d92aa951171abf9fffc3044eee8eb9d1b
Matthew-Burfield <m1.burfield@gmail.com>
2017-02-26 12:05:14 +1000

    Updated reducer

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -11,120 +11,57 @@ import defaultState from '../defaultState';
 const reducer = (state = defaultState, action) => {
   switch (action.type) {
 
-    case INCREASE_CURRENT_SET: {
-      const numExercises = this.state.data.getIn(['routines', 'fullBodyWorkout', 'exercises']).size;
-      let currentWorkout = this.state.data.get('currentWorkout');
+    case INCREASE_CURRENT_SET:
+    case NEXT_EXERCISE: {
+      const numExercises = this.state.data.getIn(['routines', 'fullBodyWorkout', 'exercises']).size - 1;
 
+      let currentWorkout = this.state.data.get('currentWorkout');
       currentWorkout += 1;
-      if (currentWorkout > numExercises - 1) {
-        currentWorkout = numExercises - 1;
-      }
+      currentWorkout = currentWorkout > numExercises ? numExercises : currentWorkout;
 
       return state.data.set('currentWorkout', currentWorkout);
     }
 
 
-    case NEXT_EXERCISE:
-    case PREVIOUS_EXERCISE:
-    case RESET_EXERCISE_TIMER:
-    case START_EXERCISE_TIMER:
-    case INCREASE_EXERCISE_TIMER:
-      break;
+    case PREVIOUS_EXERCISE: {
+      let currentWorkout = this.state.data.get('currentWorkout');
+      currentWorkout -= 1;
+      currentWorkout = currentWorkout < 0 ? 0 : currentWorkout;
+
+      return state.data.set('currentWorkout', currentWorkout);
+    }
+
+
+    case RESET_EXERCISE_TIMER: {
+      state.data.setIn(
+        ['routines', 'fullBodyWorkout', 'exercises', action.exerciseId, 'timeBetweenSets'], 180,
+      );
+      state.data.setIn(
+        ['routines', 'fullBodyWorkout', 'exercises', action.exerciseId, 'isCountingDown'], false,
+      );
+      return state.data.setIn(
+        ['routines', 'fullBodyWorkout', 'exercises', action.exerciseId, 'isInRecovery'], false,
+      );
+    }
+
+
+    case START_EXERCISE_TIMER: {
+      state.data.setIn(
+        ['routines', 'fullBodyWorkout', 'exercises', action.exerciseId, 'isCountingDown'], true,
+      );
+      return state.data.setIn(
+        ['routines', 'fullBodyWorkout', 'exercises', action.exerciseId, 'isInRecovery'], false,
+      );
+    }
+
+
+    case INCREASE_EXERCISE_TIMER: {
+      return state.data.updateIn(
+        ['routines', 'fullBodyWorkout', 'exercises', action.exerciseId, 'timer'], val => val - 1,
+      );
+    }
     default:
       return state;
   }
-  return state;
 };
 export default reducer;
-
-//
-// /**
-//  * User has clicked the button to say they have finished their current set.
-//  * Start the timer loop and increase the number of sets.
-//  */
-// handleFinishedSetBtnClk(exerciseIndex) {
-//   this.setState(prevState => ({
-//     data: prevState.data.updateIn(
-//       ['routines', 'fullBodyWorkout', 'exercises', exerciseIndex, 'currSets'], val => val + 1,
-//     ),
-//   }));
-//   this.resetTimer(exerciseIndex);
-//   this.startCountdown(exerciseIndex, true);
-//   this.createInterval(exerciseIndex, 'timeBetweenSets');
-// }
-//
-
-//
-// /**
-//  * Descrease the currentWorkout. This will rerender the screen to the next exercise.
-//  */
-// previousExercise() {
-//   let currentWorkout = this.state.data.get('currentWorkout') - 1;
-//   currentWorkout = currentWorkout < 0 ? 0 : currentWorkout;
-//
-//   this.setState(prevState => ({
-//     data: prevState.data.set('currentWorkout', currentWorkout),
-//   }));
-// }
-//
-//
-// tick(exerciseIndex, key) {
-//   const isInRecovery = this.state.data.getIn(['routines', 'fullBodyWorkout', 'exercises', exerciseIndex, 'isInRecovery']);
-//   const timeLengthOfExercise = this.state.data.getIn(['routines', 'fullBodyWorkout', 'exercises', exerciseIndex, 'timeLengthOfExerciseCounter']);
-//   const timeBetweenSets = this.state.data.getIn(['routines', 'fullBodyWorkout', 'exercises', exerciseIndex, 'timeBetweenSets']);
-//
-//   this.setState(prevState => ({
-//     data: prevState.data.updateIn(
-//       ['routines', 'fullBodyWorkout', 'exercises', exerciseIndex, key], val => val - 1,
-//     ),
-//   }));
-//
-//   if (!isInRecovery && timeLengthOfExercise <= 0) {
-//     this.handleFinishedSetBtnClk(exerciseIndex);
-//   }
-//   if (isInRecovery && timeBetweenSets <= 0) {
-//     clearInterval(this.interval);
-//     this.resetTimer(exerciseIndex);
-//   }
-// }
-//
-// resetTimer(exerciseIndex) {
-//   this.setState(prevState => ({
-//     data: prevState.data.setIn(
-//       ['routines', 'fullBodyWorkout', 'exercises', exerciseIndex, 'timeBetweenSets'], 180,
-//     ),
-//   }));
-//   this.setState(prevState => ({
-//     data: prevState.data.setIn(
-//       ['routines', 'fullBodyWorkout', 'exercises', exerciseIndex, 'isCountingDown'], false,
-//     ),
-//   }));
-//   this.setState(prevState => ({
-//     data: prevState.data.setIn(
-//       ['routines', 'fullBodyWorkout', 'exercises', exerciseIndex, 'isInRecovery'], false,
-//     ),
-//   }));
-// }
-//
-// handleStartTimerBtnClk(exerciseIndex) {
-//   this.startCountdown(exerciseIndex, false);
-//   this.createInterval(exerciseIndex, 'timeLengthOfExerciseCounter');
-// }
-//
-// startCountdown(exerciseIndex, isInRecovery = false) {
-//   this.setState(prevState => ({
-//     data: prevState.data.setIn(
-//       ['routines', 'fullBodyWorkout', 'exercises', exerciseIndex, 'isCountingDown'], true,
-//     ),
-//   }));
-//   this.setState(prevState => ({
-//     data: prevState.data.setIn(
-//       ['routines', 'fullBodyWorkout', 'exercises', exerciseIndex, 'isInRecovery'], isInRecovery,
-//     ),
-//   }));
-// }
-//
-// createInterval(exerciseIndex, counterValue) {
-//   clearInterval(this.interval);
-//   this.interval = setInterval(() => this.tick(exerciseIndex, counterValue), 1000);
-// }
