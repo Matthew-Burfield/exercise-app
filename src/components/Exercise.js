bfd4fb978168e9dd18b7c97e8aa4c71e4015e16d
Matthew-Burfield <m1.burfield@gmail.com>
2017-03-05 19:16:30 +1000

    Cleaned up timer functions

--- src/components/Exercise.js
+++ src/components/Exercise.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 
-import {
-  // increaseCurrentSet,
-  reducePreparationPeriod,
-  removePreparationPeriod,
-  reduceHoldPeriod,
-  removeHoldPeriod,
-  reduceRestPeriod,
-  removeRestPeriod,
-  addInterval,
-  createTimer,
-  clearTimer,
-} from '../actions/actions';
+// import {
+//   // increaseCurrentSet,
+//   reducePreparationPeriod,
+//   removePreparationPeriod,
+//   reduceHoldPeriod,
+//   removeHoldPeriod,
+//   reduceRestPeriod,
+//   removeRestPeriod,
+//   addInterval,
+//   createTimer,
+//   clearTimer,
+// } from '../actions/actions';
 import CountdownTimer from './CountdownTimer';
 
 /**
@@ -82,173 +82,160 @@ export const counter = function counter(exerciseId) {
 };
 
 
-class Exercise extends React.Component {
+const Exercise = ({ exercise, currentWorkout }) => {
 
-  static propTypes = {
-    currentWorkout: React.PropTypes.number,
-    exercise: React.PropTypes.shape({
-      get: React.PropTypes.func,
-      getIn: React.PropTypes.func,
-      sets: React.PropTypes.number,
-      currSets: React.PropTypes.number,
-      reps: React.PropTypes.number,
-      timeUntilNextSet: React.PropTypes.number,
-      variations: React.PropTypes.arrayOf(React.PropTypes.string),
-      selectedVariation: React.PropTypes.number,
-      weight: React.PropTypes.number,
-      timeLengthOfExercise: React.PropTypes.number,
-    }),
-    dispatch: React.PropTypes.func,
+  // const tick = () => {
+  //
+  //   const { dispatch, currentWorkout, exercise } = props;
+  //   const timer = exercise.get('timer');
+  //   const preparationPeriod = timer.get('preparationPeriod');
+  //   const holdPeriod = timer.get('holdPeriod');
+  //   const restPeriod = timer.get('restPeriod');
+  //
+  //   if (preparationPeriod !== undefined) {
+  //
+  //     if (preparationPeriod === 0) {
+  //
+  //       dispatch(removePreparationPeriod(currentWorkout));
+  //
+  //     } else {
+  //
+  //       dispatch(reducePreparationPeriod(currentWorkout));
+  //
+  //     }
+  //
+  //   } else if (holdPeriod !== undefined) {
+  //
+  //     if (holdPeriod === 0) {
+  //
+  //       dispatch(removeHoldPeriod(currentWorkout));
+  //
+  //     } else {
+  //
+  //       dispatch(reduceHoldPeriod(currentWorkout));
+  //
+  //     }
+  //
+  //   } else if (restPeriod !== undefined) {
+  //
+  //     if (restPeriod === 0) {
+  //
+  //       dispatch(removeRestPeriod(currentWorkout));
+  //
+  //     } else {
+  //
+  //       dispatch(reduceRestPeriod(currentWorkout));
+  //
+  //     }
+  //
+  //   } else {
+  //
+  //     // All counting has finished.
+  //     clearInterval(timer.get('interval'));
+  //     dispatch(clearTimer(currentWorkout));
+  //
+  //   }
+  //
+  // };
+
+
+  const handleClick = () => {
+
+    // this.props.dispatch(createTimer(this.props.currentWorkout));
+    // const interval = setInterval(tick, 1000);
+    // this.props.dispatch(addInterval(this.props.currentWorkout, interval));
+    const t = counter(currentWorkout);
+    t.createInterval();
+
   };
 
+  const timer = exercise.get('timer');
+  const backgroundColor = getBackgroundColour();
 
-  static defaultProps = {
-    currentWorkout: 0,
-    exercise: {
-      name: '',
-      sets: 0,
-      currSets: 0,
-      reps: 0,
-      get: () => {},
-    },
-    dispatch: () => {},
-  };
+  let remainingTime;
+  if (timer) {
 
+    if (timer.get('preparationPeriod') !== undefined) {
 
-  constructor(props) {
+      remainingTime = timer.get('preparationPeriod');
 
-    super(props);
-    this.tick = this.tick.bind(this);
-    this.handleClick = this.handleClick.bind(this);
+    } else if (timer.get('holdPeriod') !== undefined) {
 
-  }
+      remainingTime = timer.get('holdPeriod');
 
+    } else if (timer.get('restPeriod') !== undefined) {
 
-  tick() {
-
-    const { dispatch, currentWorkout, exercise } = this.props;
-    const timer = exercise.get('timer');
-    const preparationPeriod = timer.get('preparationPeriod');
-    const holdPeriod = timer.get('holdPeriod');
-    const restPeriod = timer.get('restPeriod');
-
-    if (preparationPeriod !== undefined) {
-
-      if (preparationPeriod === 0) {
-
-        dispatch(removePreparationPeriod(currentWorkout));
-
-      } else {
-
-        dispatch(reducePreparationPeriod(currentWorkout));
-
-      }
-
-    } else if (holdPeriod !== undefined) {
-
-      if (holdPeriod === 0) {
-
-        dispatch(removeHoldPeriod(currentWorkout));
-
-      } else {
-
-        dispatch(reduceHoldPeriod(currentWorkout));
-
-      }
-
-    } else if (restPeriod !== undefined) {
-
-      if (restPeriod === 0) {
-
-        dispatch(removeRestPeriod(currentWorkout));
-
-      } else {
-
-        dispatch(reduceRestPeriod(currentWorkout));
-
-      }
+      remainingTime = timer.get('restPeriod');
 
     } else {
 
-      // All counting has finished.
-      clearInterval(timer.get('interval'));
-      dispatch(clearTimer(currentWorkout));
+      remainingTime = 0;
 
     }
 
   }
 
 
-  handleClick() {
-
-    this.props.dispatch(createTimer(this.props.currentWorkout));
-    const interval = setInterval(this.tick, 1000);
-    this.props.dispatch(addInterval(this.props.currentWorkout, interval));
-
-  }
-
-
-  render() {
-
-    const { exercise } = this.props;
-    const timer = exercise.get('timer');
-    const backgroundColor = getBackgroundColour();
-
-    let remainingTime;
-    if (timer) {
-
-      if (timer.get('preparationPeriod') !== undefined) {
-
-        remainingTime = timer.get('preparationPeriod');
-
-      } else if (timer.get('holdPeriod') !== undefined) {
-
-        remainingTime = timer.get('holdPeriod');
-
-      } else if (timer.get('restPeriod') !== undefined) {
-
-        remainingTime = timer.get('restPeriod');
-
-      } else {
-
-        remainingTime = 0;
-
+  return (<div>
+    <div className="currentExercise" style={{ backgroundColor }}>
+      <div><p className="title"><b className="name">Name: </b>{exercise.get('name')}</p></div>
+      <div><p><b className="sets">Sets:</b> {exercise.get('currSets')}/{exercise.get('sets')}</p></div>
+      <div><p><b className="reps">Reps:</b> {exercise.get('reps')}</p></div>
+      {/* {ex.selectedVariation !== '' && ex.variations.length > 0 &&
+        <p><b>Current Variation:</b> {ex.variations[ex.selectedVariation]}</p>
+      } */}
+      {exercise.get('weight') &&
+        <div><p><b className="weights">weight:</b> {exercise.get('weight')}</p></div>
       }
+      {exercise.get('timeLengthOfExercise') !== undefined &&
+        <div><p>{exercise.get('timeLengthOfExercise')} sec hold</p></div>
+      }
+      {timer &&
+        <CountdownTimer
+          remainingTime={remainingTime}
+        />
+      }
+      {!timer &&
+        <Button
+          bsSize="large"
+          onClick={handleClick}
+        >
+          Finished Set
+        </Button>
+      }
+    </div>
+  </div>);
 
-    }
+};
+
+Exercise.propTypes = {
+  currentWorkout: React.PropTypes.number,
+  exercise: React.PropTypes.shape({
+  //   get: React.PropTypes.func,
+  //   getIn: React.PropTypes.func,
+  //   sets: React.PropTypes.number,
+  //   currSets: React.PropTypes.number,
+  //   reps: React.PropTypes.number,
+  //   timeUntilNextSet: React.PropTypes.number,
+  //   variations: React.PropTypes.arrayOf(React.PropTypes.string),
+  //   selectedVariation: React.PropTypes.number,
+  //   weight: React.PropTypes.number,
+  //   timeLengthOfExercise: React.PropTypes.number,
+  }),
+  // dispatch: React.PropTypes.func,
+};
 
 
-    return (<div>
-      <div className="currentExercise" style={{ backgroundColor }}>
-        <div><p className="title"><b className="name">Name: </b>{exercise.get('name')}</p></div>
-        <div><p><b className="sets">Sets:</b> {exercise.get('currSets')}/{exercise.get('sets')}</p></div>
-        <div><p><b className="reps">Reps:</b> {exercise.get('reps')}</p></div>
-        {/* {ex.selectedVariation !== '' && ex.variations.length > 0 &&
-          <p><b>Current Variation:</b> {ex.variations[ex.selectedVariation]}</p>
-        } */}
-        {exercise.get('weight') &&
-          <div><p><b className="weights">weight:</b> {exercise.get('weight')}</p></div>
-        }
-        {exercise.get('timeLengthOfExercise') !== undefined &&
-          <div><p>{exercise.get('timeLengthOfExercise')} sec hold</p></div>
-        }
-        {timer &&
-          <CountdownTimer
-            remainingTime={remainingTime}
-          />
-        }
-        {!timer &&
-          <Button
-            bsSize="large"
-            onClick={this.handleClick}
-          >
-            Finished Set
-          </Button>
-        }
-      </div>
-    </div>);
-
-  }
-}
+Exercise.defaultProps = {
+  currentWorkout: 0,
+  exercise: {
+    name: '',
+    sets: 0,
+    currSets: 0,
+    reps: 0,
+    get: () => {},
+  },
+  dispatch: () => {},
+};
 
 export default Exercise;
