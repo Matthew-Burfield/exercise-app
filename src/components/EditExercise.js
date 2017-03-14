d956e36b5edae478831161e65304ab34428a3108
Matthew-Burfield <m1.burfield@gmail.com>
2017-03-14 20:49:46 +1000

    Adding more to edit exercise form

--- src/components/EditExercise.js
+++ src/components/EditExercise.js
@@ -1,25 +1,31 @@
 import React from 'react';
-import { FormGroup, ControlLabel, FormControl, HelpBlock } from 'react-bootstrap';
+import { Form, Col, FormGroup, ControlLabel, FormControl, HelpBlock } from 'react-bootstrap';
 
 
-const FieldGroup = ({ id, label, help, ...props }) => {
+const FieldGroup = ({ id, label, value, help, ...props }) => {
 
   FieldGroup.propTypes = {
     id: React.PropTypes.string.isRequired,
     label: React.PropTypes.string.isRequired,
+    value: React.PropTypes.string,
     help: React.PropTypes.string,
   };
 
 
   FieldGroup.defaultProps = {
+    value: '',
     help: '',
   };
 
 
   return (
     <FormGroup controlId={id}>
-      <ControlLabel>{label}</ControlLabel>
-      <FormControl {...props} />
+      <Col componentClass={ControlLabel} xs={2}>
+        {label}
+      </Col>
+      <Col xs={10}>
+        <FormControl value={value} {...props} />
+      </Col>
       {help && <HelpBlock>{help}</HelpBlock>}
     </FormGroup>
   );
@@ -33,14 +39,26 @@ const EditExercise = ({ exercise }) => {
   };
 
   return (
-    <form>
+    <Form horizontal>
       <FieldGroup
         id="formControlsText"
         label="Name"
         type="text"
-        placeholder={exercise.name}
+        value={exercise.name}
       />
-    </form>
+      <FieldGroup
+        id="formControlsText"
+        label="Sets"
+        type="text"
+        value={exercise.sets}
+      />
+      <FieldGroup
+        id="formControlsText"
+        label="Reps"
+        type="text"
+        value={exercise.reps}
+      />
+    </Form>
   );
 };
 
